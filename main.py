d2ebdfea132dc81671489d93f0ee734e360eafb7
Mr235 <cnmr235@hotmail.com>
2017-01-10 07:33:31 +0800

    list dictionary

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ print("--------------[开始, 结束, 步长]-------------------")
 for x in range(4, 19, 4):
     print(x)
 
-
 print("--------------while-------------------")
 
 buttcrack = 5
@@ -65,16 +64,20 @@ for n in range(20):
 
 print("--------------function-------------------")
 
+
 def beef():
     print("Dayum, functions are cool!")
+
 
 def bitcoin_to_usd(btc):
     amount = btc * 527
     print(amount)
 
+
 def allowed_dating_age(my_age):
     girls_age = my_age / 2 + 7
     return girls_age
+
 
 beef()
 bitcoin_to_usd(3.3)
@@ -84,20 +87,25 @@ print("Bucky can date girls", buckys_limit, "or older")
 
 print("--------------function default argument-------------------")
 
-def get_gender(sex = "Unknown"):
+
+def get_gender(sex="Unknown"):
     if sex is "m":
         sex = "Male"
     if sex is "f":
         sex = "Female"
     print(sex)
 
+
 get_gender('m')
 get_gender('f')
 get_gender()
 
 print("--------------function argument order-------------------")
-def dumb_sentence(name = "Bucky", action = "ate", item = "tuna"):
+
+
+def dumb_sentence(name="Bucky", action="ate", item="tuna"):
     print(name, action, item)
+
 
 dumb_sentence()
 dumb_sentence("Sally", "farts", "gently")
@@ -105,12 +113,45 @@ dumb_sentence(item="awesome")
 dumb_sentence(item="awesome", action="is")
 
 print("--------------变长参数列表-------------------")
+
+
 def add_numbers(*args):
     total = 0
     for a in args:
         total += a
     print(total)
 
+
 add_numbers(5)
 add_numbers(2, 5)
 add_numbers(2, 3, 5, 7, 2)
+
+print("--------------Unpacking Arguments-------------------")
+
+
+def health_calculator(age, apples_ate, cigs_smoked):
+    answer = (100 - age) + (apples_ate * 3.5) - (cigs_smoked * 2)
+    print(answer)
+
+
+buckys_data = [27, 20, 0]
+
+health_calculator(buckys_data[0], buckys_data[1], buckys_data[2])
+health_calculator(*buckys_data)
+
+print("--------------set 用{} list 用[]-------------------")
+
+groceries = {"cereal", "milk", "starcrunch", "beer", "duct tape", "lotion", "beer"}
+print(groceries)
+
+if "milk" in groceries:
+    print("You already have milk hoss!")
+else:
+    print("Oh yea, you need milk!")
+
+print("--------------Dictionary-------------------")
+
+classmates = {"Tony": "cool but smells", "Emma": "sits behind me", "Lucy": "asks too many questions"}
+
+for k, v in classmates.items():
+    print(k, v)
